139155c82a3f1dff52aa5b8e28a462d18339c007
jijiseong <jiseong5036@gmail.com>
2023-03-19 18:55:16 +0900

    출석 체크 api 기능 수정

--- pages/api/check.ts
+++ pages/api/check.ts
@@ -3,6 +3,7 @@ import User from "@/models/User";
 import type { NextApiRequest, NextApiResponse } from "next";
 import Lecture, { IAttendance, ILecture } from "@/models/Lecture";
 import dbConnect from "@/utils/db/dbConnect";
+import Code, { ICode } from "@/models/Code";
 
 /*
   출석 부 DB 수정
@@ -19,17 +20,33 @@ export default async function handler(
 ) {
   dbConnect();
   const num = String(req.query.num);
-  const lectureName = String(req.query.lecture);
-  const week = Number(req.query.week);
-  const lecture = await Lecture.findOne({ name: lectureName });
+  const lectureId = Number(req.query.lectureId);
+  const week = Number(req.query.week) - 1;
+  const qrcode = String(req.query.qrcode);
 
-  if (!lecture) return res.status(200).json({ num: num, result: "error" });
+  const lecture = await Lecture.findOne({ id: lectureId });
+  const code: ICode | null = await Code.findOne({
+    code: qrcode,
+    lectureId,
+  });
+
+  if (!lecture) {
+    return res.status(200).json({ num: num, result: "lecture error" });
+  }
+  if (!code) {
+    return res.status(200).json({ num: num, result: "code error" });
+  }
+
+  const now = new Date().getTime();
   const attendance_list = { ...lecture.attendance };
   const attendance_copy = [...attendance_list[num]];
-  attendance_copy[week] = 0;
+  if (now - code.time < 5000) {
+    attendance_copy[week] = 0;
+  } else if (now - code.time < 1000 * 60 * 10) {
+    return res.status(200).json({ num: num, result: "QR code 유효시간 초과" });
+  }
   attendance_list[num] = attendance_copy;
   lecture.attendance = attendance_list;
-
   await lecture.save();
 
   return res.status(200).json({ num: num, result: "success" });
